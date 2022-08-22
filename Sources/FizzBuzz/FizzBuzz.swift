abd000fff61f6e32d14169916508ca23bb2c5b80
Nadia Rozhdestvenskaia <n.rozhdestvenskaya@dodopizza.com>
2022-08-23 01:13:05 +0200

    step 3: refactoring

--- Sources/FizzBuzz/FizzBuzz.swift
+++ Sources/FizzBuzz/FizzBuzz.swift
@@ -1,13 +1,14 @@
 public class FizzBuzz {
     enum Constants {
         static let fizz = "Fizz"
+        static let buzz = "Buzz"
         static let separator = "\n"
     }
     func create(_ array: [Int]) -> String {
         array
             .map { number in
                 var result = shouldBeFizz(number) ? Constants.fizz : String(number)
-                result = number % 5 == 0 ? "Buzz" : result
+                result = shouldBeBuzz(number) ? Constants.buzz : result
                 return result
             }
             .joined(separator: Constants.separator)
@@ -15,5 +16,9 @@ public class FizzBuzz {
 
     private func shouldBeFizz(_ number: Int) -> Bool {
         number % 3 == 0
+    }
+
+    private func shouldBeBuzz(_ number: Int) -> Bool {
+        number % 5 == 0
     }
 }
