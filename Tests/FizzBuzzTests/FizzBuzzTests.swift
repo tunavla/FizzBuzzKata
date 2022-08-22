4618f41b6c2a0a7ac5b15e317ee0407424b0da65
Nadia Rozhdestvenskaia <n.rozhdestvenskaya@dodopizza.com>
2022-08-23 01:41:19 +0200

    extra step 1: red

--- Tests/FizzBuzzTests/FizzBuzzTests.swift
+++ Tests/FizzBuzzTests/FizzBuzzTests.swift
@@ -17,17 +17,24 @@ final class FizzBuzzTests: XCTestCase {
         XCTAssertEqual(sut.create([1,2,4]), "1\n2\n4")
     }
 
-    func test_setNumbers_replaceNumberDiv3WithFizz() {
+    func test_setNumbers1to3_replaceNumberDiv3WithFizz() {
         XCTAssertEqual(sut.create([1,2,3]), "1\n2\nFizz")
     }
 
-    func test_setNumbers_replaceNumberDiv5WithBuzz() {
+    func test_setNumbers1to5_replaceNumberDiv5WithBuzz() {
         XCTAssertEqual(sut.create([1,2,3,4,5]), "1\n2\nFizz\n4\nBuzz")
     }
 
-    func test_setNumbers_replaceNumberDiv3and5WithFizzBuzz() {
+    func test_setNumbers1to10_replaceNumberDiv3and5WithFizzBuzz() {
         let input = Array(1...16)
         let expectedResult = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16"
+        let result = sut.create(input)
+        XCTAssertEqual(result, expectedResult)
+    }
+
+    func test_setArrayWithTreeDigitNumber_replaceDigitCountDiv3WithFizz() {
+        let input = [1, 22, 133]
+        let expectedResult = "1\n22\nFizz"
         let result = sut.create(input)
         XCTAssertEqual(result, expectedResult)
     }
